3e79070c41ea4c0dc6b5920989e9c1f510f5d58d
José Manuel Luque Burgos <luqueburgosjm@gmail.com>
2016-02-18 00:21:44 +0100

    Post

--- Facebook.py
+++ Facebook.py
@@ -10,7 +10,7 @@ import unicodedata
 # You'll need an access token here to do anything.  You can get a temporary one
 # here: https://developers.facebook.com/tools/explorer/
 
-token = 'CAAHNOVctwEQBAKygzdgUjZA8JCnrihkZAbsWPPk1WsIHZCC8ZCJWXsDjr2pL4zjH4ztMxXjuHSErrd9999bimd2DPGIUFbptFOYzuBvIJ3fzGtEV37juiJNzTks6bECi5833gy8vX8HM1ccjhyhBfFPrmkfqBTR7DrBizp2eF7KbRVhTS8ghLuNDtdiJLCkZD'
+token = 'CAAGmrmZAFYIkBAKhImP4CZAZA3EMUoFdh1TXdRX9gwYXZCeBZAoGqvzuZAiY5meaPPEfs2P65a4LsRYH5sZCsqinrUGe3B75OeLXfIbtWgTTZBzQaFIrbkKiHNQPzcul8pgXMpqdYZCN9iiQIAtwSUjp2pJjP5374PXnQsVa4GgSFZBvpQLji9l50ZCppRuYJ3eG5jv2tDs1hfBvG7hQzebyZBdI'
 
 graph = facebook.GraphAPI(token)
 profile = graph.get_object("me")
@@ -21,7 +21,7 @@ id_f = profile['id']
 
 # Info usu
 print id_f, name
-
+print "-----------------"
 # Lista amigos
 
 # con esta url hace falta permiso del token http://graph.facebook.com/v2.5/10206814498535741/friendlists
@@ -31,3 +31,19 @@ friends = graph.get_object("/me/taggable_friends")
 
 for friend in friends['data']:
     print friend['name'].encode('utf-8')
+print "----------------"
+
+# feed
+
+#curl -i -X GET \
+# "https://graph.facebook.com/v2.5/me?fields=feed&access_token=CAACEdEose0cBAJlEpVvJvEIigS8ZBJPfVfAHTIkERuW5niPndsrXcPHcxepQ3E7jAXOdRDIuqoohBZBQIkGaNgFSSPqn6s4QL6UhFu8EodosMC53qXA05ZA3oBragS3wKYDK00ZBTYZBb5sdZCY438wGxgSU8aWEQpDCOBWAFaUh1BsnVHTkcwxH9CnNGhxY3tdvfZBzPRXPZBmAmJw5cXhN"
+
+response = requests.get('https://graph.facebook.com/v2.5/me?fields=feed&access_token=CAACEdEose0cBAJlEpVvJvEIigS8ZBJPfVfAHTIkERuW5niPndsrXcPHcxepQ3E7jAXOdRDIuqoohBZBQIkGaNgFSSPqn6s4QL6UhFu8EodosMC53qXA05ZA3oBragS3wKYDK00ZBTYZBb5sdZCY438wGxgSU8aWEQpDCOBWAFaUh1BsnVHTkcwxH9CnNGhxY3tdvfZBzPRXPZBmAmJw5cXhN')
+posts = response.json()
+
+for post in posts:
+    print ("{1} = {1}".format("story", posts[post]))
+#posts = graph.get_object("/me/feed")
+#print posts
+#for post in posts['data']:
+#    print post['name'].encode('utf-8')
